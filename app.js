e47e0efb572665e054b121036e283c92a6083976
Christian Gonzalez <kgomin@gmail.com>
2021-12-30 07:10:50 +0100

    update folders an Js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let i=1;
-function changePokemon(){
+function showPokemon(){
     let container = document.getElementById('pokemon');
     container.innerHTML = `<img class="poke" src='https://tinyurl.com/ironhack-pokemons/${i}.svg'>`;
 }
@@ -11,14 +11,14 @@ let next = document.getElementById('der');
 prev.onclick = function(){
     if (i>0){
         i--;
-        changePokemon();
+        showPokemon();
     }
 }
 next.onclick = function(){
     if(i<650){
         i++;
-        changePokemon();
+        showPokemon();
     }
 }
 
-changePokemon();
+showPokemon();
