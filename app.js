fadee9ae820ade7c3daa1534e4149f6103e4d1c3
Christian Gonzalez <kgomin@gmail.com>
2022-01-12 10:40:50 +0100

    Clean comments

--- app.js
+++ app.js
@@ -1,78 +1,79 @@
 let count = 1;
 
-function render(){
-    let container = document.getElementById('pokemon');
+function render() {
+  let container = document.getElementById("pokemon");
 
-    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
+  container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
 }
 
-function right(){
-    if (count < 650){
-        count++
-        render();
-    }
+function right() {
+  if (count < 650) {
+    count++;
+    render();
+  }
 }
-function left(){
-    if (count > 1){
-        count--
-        render();
-    }
+function left() {
+  if (count > 1) {
+    count--;
+    render();
+  }
 }
 
-let body =document.querySelector('body');
-body.addEventListener('keydown',function(e){//change pokemon using arrows
-    if(e.key==='ArrowRight'){
-        right();
-    }else if(e.key==='ArrowLeft'){
-        left();
-    }
+let body = document.querySelector("body");
+body.addEventListener("keydown", function (e) {
+  //change pokemon using arrows
+  if (e.key === "ArrowRight") {
+    right();
+  } else if (e.key === "ArrowLeft") {
+    left();
+  }
 });
 
-let previous = document.getElementById('previous');
-previous.onclick = function(){left()};
-    
-let next = document.getElementById('next');
-next.onclick = function(){right()};
+let previous = document.getElementById("previous");
+previous.onclick = function () {
+  left();
+};
 
+let next = document.getElementById("next");
+next.onclick = function () {
+  right();
+};
 
-let send = document.getElementById('send');
+let send = document.getElementById("send");
 
-send.onclick = function number(x){
+send.onclick = function number(x) {
+  let container = document.getElementById("pokemon");
+  console.log(container);
 
-    let container = document.getElementById('pokemon');
-    console.log(container);
-
-    if (i.value > 0 && i.value < 650){
-
+  if (i.value > 0 && i.value < 650) {
     container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${i.value}.svg">`;
 
     count = i.value;
 
     clear();
-    }
-}
+  }
+};
 
-
-
-let enter = document.getElementById('number');
+let enter = document.getElementById("number");
 
 enter.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {  
-      number(i.value);
-    }
-  });
-  
-  let i = document.querySelector('input');
+  if (e.key === "Enter") {
+    number(i.value);
+  }
+});
 
-function clear(){ //Erase de input data
-    enter.value='';    
+let i = document.querySelector("input");
+
+function clear() {
+  //Erase de input data
+  enter.value = "";
 }
 
-  function number(x){
-    let container = document.getElementById('pokemon');
-    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${i.value}.svg">`;
-    count = i.value;   
-    clear();
-  }
-clear();//Erase the data input at refresh the page
+function number(x) {
+  let container = document.getElementById("pokemon");
+  container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${i.value}.svg">`;
+  count = i.value;
+  clear();
+}
+clear(); //Erase the data input at refresh the page
 render();
