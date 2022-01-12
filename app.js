540c502b9ae6a4602bb41d91349e98ae82ee1d1a
Christian Gonzalez <kgomin@gmail.com>
2022-01-12 10:36:15 +0100

    Update version

--- app.js
+++ app.js
@@ -1,24 +1,78 @@
+let count = 1;
 
-let i=1;
-function showPokemon(){
+function render(){
     let container = document.getElementById('pokemon');
-    container.innerHTML = `<img class="poke" src='https://tinyurl.com/ironhack-pokemons/${i}.svg'>`;
+
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${count}.svg">`;
 }
 
-let prev = document.getElementById('izq');
-let next = document.getElementById('der');
-
-prev.onclick = function(){
-    if (i>0){
-        i--;
-        showPokemon();
+function right(){
+    if (count < 650){
+        count++
+        render();
     }
 }
-next.onclick = function(){
-    if(i<650){
-        i++;
-        showPokemon();
+function left(){
+    if (count > 1){
+        count--
+        render();
     }
 }
 
-showPokemon();
+let body =document.querySelector('body');
+body.addEventListener('keydown',function(e){//change pokemon using arrows
+    if(e.key==='ArrowRight'){
+        right();
+    }else if(e.key==='ArrowLeft'){
+        left();
+    }
+});
+
+let previous = document.getElementById('previous');
+previous.onclick = function(){left()};
+    
+let next = document.getElementById('next');
+next.onclick = function(){right()};
+
+
+let send = document.getElementById('send');
+
+send.onclick = function number(x){
+
+    let container = document.getElementById('pokemon');
+    console.log(container);
+
+    if (i.value > 0 && i.value < 650){
+
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${i.value}.svg">`;
+
+    count = i.value;
+
+    clear();
+    }
+}
+
+
+
+let enter = document.getElementById('number');
+
+enter.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {  
+      number(i.value);
+    }
+  });
+  
+  let i = document.querySelector('input');
+
+function clear(){ //Erase de input data
+    enter.value='';    
+}
+
+  function number(x){
+    let container = document.getElementById('pokemon');
+    container.innerHTML = `<img class="poke" src="https://tinyurl.com/ironhack-pokemons/${i.value}.svg">`;
+    count = i.value;   
+    clear();
+  }
+clear();//Erase the data input at refresh the page
+render();
